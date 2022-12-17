1b10f76c4d6202633e5b7941f0c0584584ab2534
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2022-12-16 23:00:42 -0700

    Updated Maximum Subarray
    Needs to fix

--- Arrays/Medium/Maximum Subarray.py
+++ Arrays/Medium/Maximum Subarray.py
@@ -6,7 +6,17 @@ class Solution:
     subarray
     which has the largest sum and return its sum.
         """
+        # two pointer method
         highest = nums[0]
         curSum = 0
- # Complete later
+        lp, rp = 0, 1
+        while lp<= len(nums) and rp <= len(nums):
+            curSum = sum(nums[lp:rp])
+            if curSum < 0:
+                curSum = 0
+                lp = rp
+            highest = max(highest,curSum)  
+            rp += 1      
+        return highest
+ # works for all except all neg values
         
